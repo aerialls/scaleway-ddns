de10508cb46c6848d71852e2bd698aecd5965218
Julien Brochet <julien@aerialls.eu>
2022-09-04 11:06:54 +0200

    feat(version): add runtime Golang version

--- cmd/scaleway-ddns/version.go
+++ cmd/scaleway-ddns/version.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/spf13/cobra"
 )
 
 var (
-	version = "undefined"
-	commit  = "dev"
-	date    = "undefined"
+	version       = "undefined"
+	commit        = "dev"
+	date          = "undefined"
+	golangVersion = runtime.Version()
 )
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Prints the version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("scaleway-ddns %s (commit %s, built at %s)\n", version, commit, date)
+		fmt.Printf("scaleway-ddns %s (%s, commit %s, built at %s)\n", version, golangVersion, commit, date)
 	},
 }
 
